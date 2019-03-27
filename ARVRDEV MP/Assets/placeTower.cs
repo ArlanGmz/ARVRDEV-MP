0d70923415ddd625fbd87b1af6e3497dacea201c
Arlan Gomez <garlanross@gmail.com>
2019-03-27 14:26:07 +0800

    files

---
+++ ARVRDEV MP/Assets/placeTower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placeTower : MonoBehaviour
+{
+    public Transform spawnpos;
+    public GameObject[] spawnee;
+    public Quaternion spawnRot;
+    private float start = 0f;
+    private float limit = 10f;
+    private bool flag = true;
+    RaycastHit hit;
+    private Vector3 v3;
+    // Update is called once per frame
+    void Update()
+    {
+        if (start == 0)
+        {
+            start += Time.deltaTime;
+
+        }
+        else if (start == limit)
+        {
+            flag = false;
+            Reset();
+        }
+
+    }
+
+    public void Reset()
+    {
+        v3 = new Vector3(spawnpos.transform.position.x, spawnpos.transform.position.y, spawnpos.transform.position.z);
+
+    }
+}
